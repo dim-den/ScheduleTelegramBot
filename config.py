e44f9103bdb09f59473eb022c0be3ff88acd0f53
Dmitriy <dimden999@gmail.com>
2022-02-11 10:55:24 +0300

    Schedule updated, fixed some bugs causing app termination, added script for notifying bot users, some more fixes

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # - *- coding: utf- 8 - *-
 
-token = '1698480311:AAH-pI41CQQFbbhK0oSSOhyHSWaHyERxIzg'
-#token = '1740234871:AAGPnAPSwJyrQku4rm3LRRx6VrDJ1DLN1_k' # test bot
+token = 'token'
+#token = 'token' # test bot
